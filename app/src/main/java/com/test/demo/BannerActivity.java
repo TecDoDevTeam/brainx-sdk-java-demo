68aa67a545ac922d85e7eccc031e270b4974365a
nelson.ding <nelson.ding@tec-do.com>
2025-06-05 14:48:36 +0800

    update to 2013

--- app/src/main/java/com/test/demo/BannerActivity.java
+++ app/src/main/java/com/test/demo/BannerActivity.java
@@ -27,11 +27,11 @@ public class BannerActivity extends AppCompatActivity implements TDBannerLoadLis
     private void initView() {
         container = findViewById(R.id.container_banner);
         findViewById(R.id.btn_back).setOnClickListener(view -> finish());
-        findViewById(R.id.btn_320_50).setOnClickListener(view -> TDBanner.load(DemoActivity.BANNER_320_50_UNIT_ID, new TDBannerConfig(TDBannerConfig.BannerSize.W_320_H_50), BannerActivity.this));
-        findViewById(R.id.btn_300_250).setOnClickListener(view -> TDBanner.load(DemoActivity.BANNER_300_250_UNIT_ID, new TDBannerConfig(TDBannerConfig.BannerSize.W_300_H_250), BannerActivity.this));
-        findViewById(R.id.btn_320_90).setOnClickListener(view -> TDBanner.load(DemoActivity.BANNER_320_90_UNIT_ID, new TDBannerConfig(TDBannerConfig.BannerSize.W_320_H_90), BannerActivity.this));
-        findViewById(R.id.btn_728_90).setOnClickListener(view -> TDBanner.load(DemoActivity.BANNER_728_90_UNIT_ID, new TDBannerConfig(TDBannerConfig.BannerSize.W_728_H_90), BannerActivity.this));
-        findViewById(R.id.btn_800_600).setOnClickListener(view -> TDBanner.load(DemoActivity.BANNER_800_600_UNIT_ID, new TDBannerConfig(TDBannerConfig.BannerSize.W_800_H_600), BannerActivity.this));
+        findViewById(R.id.btn_320_50).setOnClickListener(view -> TDBanner.load(DemoActivity.BANNER_320_50_UNIT_ID, new TDBannerConfig(), BannerActivity.this));
+        findViewById(R.id.btn_300_250).setOnClickListener(view -> TDBanner.load(DemoActivity.BANNER_300_250_UNIT_ID, new TDBannerConfig(), BannerActivity.this));
+        findViewById(R.id.btn_320_90).setOnClickListener(view -> TDBanner.load(DemoActivity.BANNER_320_90_UNIT_ID, new TDBannerConfig(), BannerActivity.this));
+        findViewById(R.id.btn_728_90).setOnClickListener(view -> TDBanner.load(DemoActivity.BANNER_728_90_UNIT_ID, new TDBannerConfig(), BannerActivity.this));
+        findViewById(R.id.btn_800_600).setOnClickListener(view -> TDBanner.load(DemoActivity.BANNER_800_600_UNIT_ID, new TDBannerConfig(), BannerActivity.this));
     }
 
     @Override
